4a02244448b69235e62ab273688af51558e40937
CharlesScrivens <cscrivens@horizon.csueastbay.edu>
2024-02-01 16:05:14 -0800

    Added connection to Discord API

--- index.js
+++ index.js
@@ -2,3 +2,20 @@
 require('dotenv').config();
 
 // Get ready to connect to the Discord API
+const {Client, GatewayIntentBits} = require('discord.js');
+const client = new Client({intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent
+]})
+
+// Prepare connection to OpenAI API
+const {Configuration, OpenAIApi} = require('openai');
+const configuration = new Configuration({
+    organization: process.env.OPENAI_ORG,
+    apiKey: process.env.OPENAI_KEY
+});
+
+const openai = new OpenAIApi(configuration);
+
+// Check for when a message is sent
