2598fdd224f0450a6edf348d97d671e2bc9905f4
CharlesScrivens <cscrivens@horizon.csueastbay.edu>
2024-02-01 16:54:02 -0800

    Changed to handle new API

--- index.js
+++ index.js
@@ -1,34 +1,5 @@
 // Goal: Create a discord bot with the OpenAI API
 require('dotenv').config();
+const { Client } = require(`discord.js`);
+const { OpenAI } = require(`openai`);
 
-// Get ready to connect to the Discord API
-const {Client, GatewayIntentBits} = require('discord.js');
-const client = new Client({intents: [
-    GatewayIntentBits.Guilds,
-    GatewayIntentBits.GuildMessages,
-    GatewayIntentBits.MessageContent
-]})
-
-// Prepare connection to OpenAI API
-// const {Configuration, OpenAIApi} = require('openai');
-// const configuration = new Configuration({
-//     organization: process.env.OPENAI_ORG,
-//     apiKey: process.env.OPENAI_KEY
-// });
-
-// const openai = new OpenAIApi(configuration);
-
-// Check for when a message is sent
-client.on('messageCreate', async function(message){
-    try {
-        if(message.author.bot) return;
-        console.log(message.content);
-        message.reply(`You said: ${message.content}`);
-    } catch (err){
-        console.log(err);
-    }
-});
-
-// Log the bot into Discord
-client.login(process.env.DISCORD_TOKEN);
-console.log("BOT IS ONLINE");
