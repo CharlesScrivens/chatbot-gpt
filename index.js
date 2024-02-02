4d4ef5b8455da2890a83e89f08687b31b737c325
CharlesScrivens <cscrivens@horizon.csueastbay.edu>
2024-02-01 17:11:26 -0800

    Added login to discord

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
 // Goal: Create a discord bot with the OpenAI API
-require('dotenv').config();
+require('dotenv/config');
 const { Client } = require(`discord.js`);
 const { OpenAI } = require(`openai`);
+
+const client = new Client({
+    intents: [`Guilds`, `GuildMembers`, `GuildMessages`, `MessageContent`]
+});
+
+client.on(`ready`, () => {
+    console.log(`The bot is on`);
+});
+
+client.login(process.env.DISCORD_TOKEN);
 
